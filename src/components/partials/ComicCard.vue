1731bfe019662f7fe3409b5fe482aadbf0777ce7
Marco Sandri <sandrimarco98@gmail.com>
2022-02-22 18:38:36 +0100

    Comics

---
+++ src/components/partials/ComicCard.vue
@@ -0,0 +1,59 @@
+<template>
+    
+    <div class="comicCard">
+            <a href="#">
+                <div class="comicImage">
+                    <img :src="url" :alt="name">
+                </div>
+                <div class="text">
+                    {{name}}
+                </div>
+            </a>
+    </div>
+
+
+</template>
+
+<script>
+
+export default {
+    name : "ComicCard",
+    props: {
+        url : String,
+        name : String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/style/general.scss";
+    @import "../../assets/style/colors.scss";
+
+    .comicCard {
+        margin: 2rem auto;
+        width: 220px;
+
+        a {
+            text-decoration: none;
+
+            .comicImage {
+                height: 220px;
+                width: 220px;
+
+                img{
+                    object-fit: cover;
+                    object-position: top;
+
+                    height: 100%;
+                    width: 100%;
+                }
+            }
+
+            .text {
+                color: #fff;
+                margin-top: 1rem;
+                text-transform: uppercase;
+            }
+        }
+    }
+</style>
---
+++ src/components/partials/JumboComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="jumbotron">
+        <img src="../../assets/img/jumbotron.jpg" alt="">
+    </div>
+</template>
+
+<script>
+
+export default {
+    name : "JumboComponent"
+}
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/style/general.scss";
+
+    .jumbotron {
+        height: 400px;
+        
+        img{
+            object-fit: cover;
+            object-position: top;
+
+            height: 100%;
+            width: 100%;
+        }
+    }
+</style>
